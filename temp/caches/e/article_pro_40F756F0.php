a4b31192ec59c6b157edc487936890372c2300db
pacy <pacy@163.com>
2017-06-14 22:44:12 +0800

    new

---
+++ temp/caches/e/article_pro_40F756F0.php
@@ -0,0 +1,110 @@
+<?php exit;?>a:3:{s:8:"template";a:5:{i:0;s:54:"/usr/share/nginx/ecshop/themes/default/article_pro.dwt";i:1;s:62:"/usr/share/nginx/ecshop/themes/default/library/page_header.lbi";i:2;s:58:"/usr/share/nginx/ecshop/themes/default/library/ur_here.lbi";i:3;s:55:"/usr/share/nginx/ecshop/themes/default/library/help.lbi";i:4;s:62:"/usr/share/nginx/ecshop/themes/default/library/page_footer.lbi";}s:7:"expires";i:1497222654;s:8:"maketime";i:1497219054;}<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="Generator" content="ECSHOP v2.7.3" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="Keywords" content="" />
+<meta name="Description" content="" />
+<title>用户协议_ECSHOP演示站 - Powered by ECShop</title>
+<link rel="shortcut icon" href="favicon.ico" />
+<link rel="icon" href="animated_favicon.gif" type="image/gif" />
+<link href="themes/default/style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="js/common.js"></script></head>
+<body>
+<script type="text/javascript">
+var process_request = "正在处理您的请求...";
+</script>
+<div class="block clearfix">
+ <div class="f_l"><a href="index.php" name="top"><img src="themes/default/images/logo.gif" /></a></div>
+ <div class="f_r log">
+   <ul>
+   <li class="userInfo">
+   <script type="text/javascript" src="js/transport.js"></script><script type="text/javascript" src="js/utils.js"></script>   <font id="ECS_MEMBERZONE">554fcae493e564ee0dc75bdf2ebf94camember_info|a:1:{s:4:"name";s:11:"member_info";}554fcae493e564ee0dc75bdf2ebf94ca </font>
+   </li>
+      </ul>
+ </div>
+</div>
+<div  class="blank"></div>
+<div id="mainNav" class="clearfix">
+  <a href="index.php" class="cur">首页<span></span></a>
+  </div>
+<div id="search"  class="clearfix">
+  <div class="keys f_l">
+   <script type="text/javascript">
+    
+    <!--
+    function checkSearchForm()
+    {
+        if(document.getElementById('keyword').value)
+        {
+            return true;
+        }
+        else
+        {
+            alert("请输入搜索关键词！");
+            return false;
+        }
+    }
+    -->
+    
+    </script>
+      </div>
+  <form id="searchForm" name="searchForm" method="get" action="search.php" onSubmit="return checkSearchForm()" class="f_r"  style="_position:relative; top:5px;">
+   <select name="category" id="category" class="B_input">
+      <option value="0">所有分类</option>
+          </select>
+   <input name="keywords" type="text" id="keyword" value="" class="B_input" style="width:110px;"/>
+   <input name="imageField" type="submit" value="" class="go" style="cursor:pointer;" />
+   <a href="search.php?act=advanced_search">高级搜索</a>
+   </form>
+</div>
+<div class="block box">
+ <div id="ur_here">
+  当前位置: <a href=".">首页</a> <code>&gt;</code> 用户协议 </div>
+</div>
+<div class="blank"></div>
+<div class="block">
+  <div class="box">
+   <div class="box_1">
+    <h3><span>用户协议</span></h3>
+    <div class="boxCenterList">
+          </div>
+   </div>
+  </div>
+  <div class="blank5"></div>
+</div>
+<div class="blank5"></div>
+<div class="block">
+  <div class="box">
+   <div class="helpTitBg clearfix">
+    <br />
+<b>Warning</b>:  preg_replace(): The /e modifier is no longer supported, use preg_replace_callback instead in <b>/usr/share/nginx/ecshop/includes/cls_template.php</b> on line <b>300</b><br />
+   </div>
+  </div>  
+</div>
+<div class="blank"></div>
+<div class="blank"></div>
+<div id="bottomNav" class="box">
+ <div class="box_1">
+  <div class="bNavList clearfix">
+   <div class="f_l">
+      </div>
+   <div class="f_r">
+   <a href="#top"><img src="themes/default/images/bnt_top.gif" /></a> <a href="index.php"><img src="themes/default/images/bnt_home.gif" /></a>
+   </div>
+  </div>
+ </div>
+</div>
+<div class="blank"></div>
+<div id="footer">
+ <div class="text">
+ &copy; 2005-2017 ECSHOP 版权所有，并保留所有权利。<br />
+                                                                                     <br />
+    554fcae493e564ee0dc75bdf2ebf94caquery_info|a:1:{s:4:"name";s:10:"query_info";}554fcae493e564ee0dc75bdf2ebf94ca<br />
+  <a href="http://www.ecshop.com" target="_blank" style=" font-family:Verdana; font-size:11px;">Powered&nbsp;by&nbsp;<strong><span style="color: #3366FF">ECShop</span>&nbsp;<span style="color: #FF9966">v2.7.3</span></strong></a>&nbsp;<a href="http://www.ecshop.com/license.php?product=ecshop_b2c&url=http%3A%2F%2Fecshop.createclouds.cn%2F" target="_blank"
+>&nbsp;&nbsp;Licensed</a><br />
+        <div align="left"  id="rss"><a href=""><img src="themes/default/images/xml_rss2.gif" alt="rss" /></a></div>
+ </div>
+</div>
+</body>
+</html>
